cbe2618e36e17b81c60178ba7c39c9cad193ee6a
Diák <petrik@gmail.com>
2026-01-14 12:01:48 +0100

    mukodik maga a kod es minden

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 function init(){
   const form = document.getElementById("regisztralas")
   const uzen = document.getElementById("uzenet")
+
+  form.addEventListener("submit", function(e){
+    e.preventDefault();
+    form.reset();            
+    form.style.display = "none";
+    uzen.style.display = "block";
+  })
+
 }
 
 
-document.addEventListener("DOMContentLoaded", init())
+document.addEventListener("DOMContentLoaded", init)
